e7c0476d5199cd2eab387b62ee552073116b66cf
Saman833 <s.ahmadifar2000@gmail.com>
2025-04-24 14:31:18 +0000

    .

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -36,7 +36,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "https://edufrontend.up.railway.app"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "production"
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
